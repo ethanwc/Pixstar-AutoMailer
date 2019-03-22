7b69fa13c89e4002d98f0a5cf4549153d0bb5890
ethanwc <ethanwc@u.washington.edu>
2019-03-22 14:15:13 -0700

    init commit

---
+++ app/src/main/java/ethanwc/tcss450/uw/edu/autopixstar/MainActivity.java
@@ -0,0 +1,13 @@
+package ethanwc.tcss450.uw.edu.autopixstar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
